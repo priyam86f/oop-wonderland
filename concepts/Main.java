5f3b910971670e6975ca46dc95dc3cbdc13a9739
Priyam <priyamatwork24@gmail.com>
2024-01-09 16:05:04 +0530

    hosting

---
+++ concepts/Main.java
@@ -0,0 +1,7 @@
+public class Main{
+    public static void main(String[] args){
+        TextBox textbox1 = new TextBox();
+        textbox1.setText("box1");
+        System.out.println(textbox1.text);
+    }
+}
