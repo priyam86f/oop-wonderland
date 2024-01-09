315378d3db8c696aa8f700c972c5d87a62f949b4
Priyam <priyamatwork24@gmail.com>
2024-01-09 17:37:06 +0530

    encapsulation+Error handling

--- concepts/encSalary.java
+++ concepts/encSalary.java
@@ -1,8 +1,31 @@
 public class encSalary {
-    public int baseSalary;
-    public int per_hour_pay;
+    private int baseSalary;
+    private int per_hour_pay;
 
     public int calculateSal(int extrahours){
      return baseSalary + (extrahours*per_hour_pay);
     }
+
+     //getters and setters
+     public void setBaseSalary(int baseSalary){
+        if(baseSalary<=0)
+        throw new IllegalArgumentException("Salary cannot be negative or zero.");
+
+        this.baseSalary=baseSalary;
+     }
+
+     public int getBaseSalary(){
+        return baseSalary;
+     }
+
+     public void setPayPerHour(int per_hour_pay){
+        if(per_hour_pay<=0)
+        throw new IllegalArgumentException("Per hour pay cannot be negative or zero.");
+
+        this.per_hour_pay=per_hour_pay;
+     }
+
+     public int getPayPerHour(){
+        return per_hour_pay;
+     }
 }
