6620b7b436afb1ba1b32198864bd33bacd7c7037
Priyam <priyamatwork24@gmail.com>
2024-01-09 16:03:15 +0530

    implementing a class

---
+++ concepts/TextBox.java
@@ -0,0 +1,13 @@
+public class TextBox{
+    //fields
+    public String text;
+
+    //methods
+    public void setText(String text){
+        this.text=text;
+    }
+
+    public void clear(){
+        this.text="";
+    }
+}
